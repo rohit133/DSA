4fc1e316f7e87bafaf3c388250798eab0e2552ec
rohit133 <rohityou000@gmail.com>
2024-05-16 21:17:27 +0530

    Updates

---
+++ Array/Notes.py
@@ -0,0 +1,14 @@
+# Creating an Array 
+
+array1 = [] # Creatign an empty array. 
+array2 = [1,2,3,4,5] # Creating an Array with only integer values
+array3 = ['a','b','c','d','e'] # Creating an array with Char values
+array4 = ['Hello', 'How', 'are', 'you' ] # Creating an array with String values
+
+
+
+
+
+
+
+
