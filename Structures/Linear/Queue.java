2347fa85f2cd08a08fffcfddd78b09fd12ac1aef
rohit133 <rohityou000@gmail.com>
2024-09-25 16:25:10 +0530

    Linear Data structures

--- Structures/Linear/Queue.java
+++ Structures/Linear/Queue.java
@@ -1,5 +1,82 @@
 package Structures.Linear;
 
 public class Queue {
-    
+    private int rear, front;
+    private int maxSize; // maximum size of the stack
+    private int[] queue;
+
+    public Queue(int size) {
+        this.maxSize = size;
+        this.queue = new int[maxSize];
+        this.rear = -1;
+        this.front = -1;
+    }
+
+    public void enQueue(int value) {
+        if (IsFull()) {
+            System.out.println("Queue is full");
+        } else {
+            if (front == -1) {
+                front = 0;
+            }
+            queue[++rear] = value;
+        }
+    }
+
+    public int deQueue() {
+        if (IsEmpty()) {
+            System.out.println("Queue is Empty");
+            return -1;
+        } else {
+            if (front >= rear) {
+                front = -1;
+                rear = -1;
+            }
+            return front++;
+        }
+    }
+
+    public void printQueue() {
+        if (IsEmpty()) {
+            System.out.println("Queue is Empty");
+            return;
+        } else {
+            for (int i = front; i <= rear; i++) {
+                System.out.println(queue[i]);
+            }
+        }
+    }
+
+    public Boolean IsEmpty() {
+        if (front == -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean IsFull() {
+        if (front == 0 && rear == maxSize - 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue(5);
+
+        System.out.println(queue.IsEmpty());
+        queue.enQueue(10);
+        queue.enQueue(12);
+        queue.enQueue(14);
+        queue.enQueue(16);
+        queue.enQueue(18);
+        System.out.println(queue.IsFull());
+
+        queue.printQueue();
+        queue.deQueue();
+        System.out.println("After Deleting the Element from the Queue!");
+        queue.printQueue();
+
+    }
+
 }
--- Structures/Linear/Stack.java
+++ Structures/Linear/Stack.java
@@ -1,5 +1,89 @@
 package Structures.Linear;
 
 public class Stack {
+    private int top;
+    private int maxSize;    // maximum size of the stack
+    private int[] stackArray;   // array to store stack elements
+
+    public Stack(int size){
+        this.maxSize = size;
+        this.stackArray =  new int[maxSize];
+        this.top = -1;
+    }
+
+    public void push(int value){
+        if(IsFull()){
+            System.out.println("Stack Overflow cannot perform Push :" +value);
+        } else {
+            stackArray[++top] = value;
+        }
+    }
+
+    public int pop(){
+        if(IsEmpty()){
+            System.out.println("Stack UnderFlow cannot perform Pop!");
+            return -1;
+        } else {
+            return stackArray[top--];
+        }
+    }
+
+    public int peek(){
+        if(IsEmpty()){
+            System.out.println("Stack UnderFlow cannot perform Peek!");
+            return -1;
+        } else {
+            return stackArray[top];
+        }
+    }
+
+    public void print(){
+        if(IsEmpty()){
+            System.out.println("Stack is Empty!");
+        } else {
+            for(int i = top; i >= 0; i--){
+                System.out.println(stackArray[i]);
+            }
+        }
+
+
+    }
+    public Boolean IsEmpty(){
+        if(top == -1){
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean IsFull(){
+        if(top == maxSize - 1){
+            return true;
+        }
+        return false;
+    }
+
+
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+        System.out.println(stack.IsEmpty());
+        stack.push(10);
+        stack.push(12);
+        stack.push(14);
+        stack.push(16);
+        stack.push(18);
+        stack.print();
+        stack.pop();
+        System.out.println("After Poping the element");
+        stack.print();
+        System.out.println(stack.IsFull());
+        System.out.println("Peeking on Stack");
+        System.out.println(stack.peek());
+
+
+
+
+
+
+    }
     
 }
