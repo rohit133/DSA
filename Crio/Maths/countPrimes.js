01452952f46aff51906ee81440fc3c3362452017
rohit133 <rohityou000@gmail.com>
2025-01-05 08:55:42 +0530

    New Prime Number

--- Crio/Maths/countPrimes.js
+++ Crio/Maths/countPrimes.js
@@ -1,18 +1,13 @@
+function isPrime(n){
+    if(n <= 1) return false;
+    for(let i = 2; i < n; i++)
+        if(n % i == 0) return false;
+    return true;
+}  
 function countPrimes(n) {
-    let primes = [];
-    for (let i = 2; i < n; i++) {
-        let isPrime = true;
-        for (let j = 2; j * j <= i; j++) {
-            if (i % j === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-        if (isPrime) {                          
-            primes.push(i);
-        }
-    }
-    return primes.length;
+    let result = [];
+    for(let i = 2; i < n; i++)
+        if(isPrime(i)) result.push(i);
+    return result.length;
 }
-
-console.log(countPrimes(10));
+console.log(countPrimes(5));
