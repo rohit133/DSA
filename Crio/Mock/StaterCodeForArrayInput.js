f1a22e6380b4235ea86a491b7d12ddb1ad4226d4
rohit133 <rohityou000@gmail.com>
2025-02-03 15:56:40 +0530

    mock stater code

---
+++ Crio/Mock/StaterCodeForArrayInput.js
@@ -0,0 +1,50 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.replace(/\s+/g, " ").trim();
+    });
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function readIntArr() {
+    let str = readLine();
+    str = str.split(" ");
+    let arr = [];
+    for ( let i = 0; i < str.length; i++ ) {
+        arr.push(parseInt(str[i], 10));
+    }
+    return arr;
+}
+
+function print(x) {
+    process.stdout.write(x + "");
+}
+
+
+// Define your function and the function parameters here
+
+
+
+function main() {
+    let n = parseInt(readLine());
+    let arr = readIntArr();
+
+    let firstIndex = 0;
+    print(firstIndex);
+}
+
---
+++ Crio/Mock/StaterCodeForSingleInput.js
@@ -0,0 +1,44 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.replace(/\s+/g, " ").trim();
+    });
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function readIntArr() {
+    let str = readLine();
+    str = str.split(" ");
+    let arr = [];
+    for ( let i = 0; i < str.length; i++ ) {
+        arr.push(parseInt(str[i], 10));
+    }
+    return arr;
+}
+
+function print(x) {
+    process.stdout.write(x + "");
+}
+
+
+
+function main() {
+    let n = parseInt(readLine(), 10);
+    let result = 0;
+    console.log(result);
+}
