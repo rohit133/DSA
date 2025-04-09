f99f98a04ef36a06c1b41548e46c68102c6d1c2a
rohit133 <rohityou000@gmail.com>
2025-04-09 18:51:28 +0530

    83. Remove Duplicates from Sorted List --leetcode Problem

---
+++ LinkedList/Questions/DSA-Problems/RemoveDuplicate.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var deleteDuplicates = function (head) {
+    let temp = head;
+    while(temp){
+        if(temp.next !== null && temp.val === temp.next.val){
+            temp.next = temp.next.next;
+        }
+        temp = temp.next;
+    }
+    return head;
+};
